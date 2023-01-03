68f9ef4f65416ca9bed94132701bc54beea2ce6b
Glenn <121842204+GlenAguh@users.noreply.github.com>
2023-01-03 20:54:30 +0800

    Menambahkan Variabel

---
+++ 01 - Variabel/variabel.py
@@ -0,0 +1,42 @@
+# Variabel adalah tempat penyimpan data
+
+# Menaruh Assigment / Nilai
+from tkinter import dialog
+
+
+a = 10
+b = 5
+glenn = 1000
+
+# Pemanggilan pertama
+print ("Nilai a =", a)
+print ("Nilai b =", b)
+print ("Glenn =", glenn)
+
+# Penamaan
+nilai_Q = 100  # underscore boleh
+glenn04 = 90  #Juga boleh
+vvt10 = 80 # Apasih yg gk boleh
+
+# Pemanggil keduaa
+print ("Nilai vvt10 =",vvt10 )
+vvt10 = "Apasih yg gk bolehh"
+print ("Nilai vvt10 =",vvt10 )
+
+print (" Glenn =",glenn04)
+glenn04 = "Noo Counter"
+print ("Glenn =",glenn04)
+
+print ("Vvt= ",vvt10)
+vvt10 = "Infinite Beauty"
+print (" Vvt =",vvt10)
+
+# Assigment indirect
+vvt10 = "Glenn"
+glenn04 = "Vt"
+print ("Vt = ",vvt10)
+print ("Glenn = ",glenn04)
+
+
+
+
