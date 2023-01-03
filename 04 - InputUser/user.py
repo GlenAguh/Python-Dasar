123dbdd4b87cd546566d734c77fee385fcbbaf52
Glenn <121842204+GlenAguh@users.noreply.github.com>
2023-01-03 20:56:50 +0800

    Menambahkan Input User

---
+++ 04 - InputUser/user.py
@@ -0,0 +1,28 @@
+# Mengambil Input Dari User
+ 
+
+
+# Data yg di masukkan pasti string
+data = input ("Masukkan data : ")
+print ("Data = ", data, ",type=",type(data))
+
+print ("")
+print ("++++++++++++++++++")
+print("")
+
+# int
+angka = int (input("Masuukan Angka : "))
+print ("Data = ", angka, ",type=",type(angka))
+
+
+print ("")
+print ("++++++++++++++++++")
+print("")
+
+# bool
+biner = bool(int(input("masukkan nilai = ")))
+print ("Nilai = ", biner, ",type=",type(biner))
+
+
+
+
